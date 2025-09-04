2f0cce9a2e886b3d0315c66cd13b53e5ecf1cab7
lazikiri <lazikiri@users.noreply.github.com>
2025-09-04 18:36:55 -0400

    install.sh: Added screenshot packages

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ if ! which paru >/dev/null; then
     echo "Paru installed."
 fi
 
-paru -S hyprland alacritty waybar ttf-cousine-nerd python-pywal16-git pavucontrol pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber xdg-desktop-portal-hyprland dunst hyprpolkitagent fish fastfetch eza rofi-wayland swww --needed --noconfirm
+paru -S hyprland alacritty waybar ttf-cousine-nerd python-pywal16-git pavucontrol pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber dunst hyprpolkitagent fish fastfetch eza rofi-wayland swww xdg-desktop-portal-hyprland hyprshot hyprpicker --needed --noconfirm
 
 mv /tmp/hyprland-dots/.config/* "$HOME/.config/"
 
