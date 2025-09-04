35ffe9ea4040e86495da4265159045129e15f91a
lazikiri <lazikiri@users.noreply.github.com>
2025-09-04 18:36:54 -0400

    add Fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+function fish_greeting
+    fastfetch
+end
+
+alias ls='eza -al --color=always --group-directories-first --icons'
+alias lt='eza -aT --color=always --group-directories-first --icons'
+
+alias grubupdate='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
