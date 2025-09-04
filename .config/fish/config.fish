055d8952c2050a911165c118e706f00912f0b1fd
lazikiri <lazikiri@users.noreply.github.com>
2025-09-04 18:36:56 -0400

    fish: Added more shell aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,13 @@ end
 alias ls='eza -al --color=always --group-directories-first --icons'
 alias lt='eza -aT --color=always --group-directories-first --icons'
 
-alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+alias cat='bat --style=full'
+alias fzf='fzf --style full --color 16 --layout=reverse --preview="bat -p --color=always {}"'
+alias lsblk='lsblk | bat --style=plain -l conf'
+
+alias installpkg='paru -Slq | fzf --multi --preview "paru -Si {1}" | xargs -ro paru -S'
+alias uninstallpkg='paru -Qq | fzf --multi --preview "paru -Si {1}" | xargs -ro paru -Rns'
+alias update='paru -Syu'
+
+alias grubupdate='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias cleanup='sudo paru -Rns (paru -Qtdq)'
