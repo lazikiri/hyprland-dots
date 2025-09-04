2e60326e5bb5c4aa50bd2cd2269f01e34c7487c2
lazikiri <lazikiri@users.noreply.github.com>
2025-09-04 18:36:54 -0400

    config.fish: Change bind

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,5 @@ end
 alias ls='eza -al --color=always --group-directories-first --icons'
 alias lt='eza -aT --color=always --group-directories-first --icons'
 
-alias grubupdate='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
