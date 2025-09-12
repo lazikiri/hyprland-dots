e8f887d247339572c77a240f88676b6f5f2345a3
lazikiri <lazikiri@users.noreply.github.com>
2025-09-12 01:45:37 -0400

    Improved install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 if [ "$(whoami)" = "root" ]; then
-    echo "Do not run as root"
-    exit 1
+  echo "Do not run as root"
+  exit 1
 fi
 
 if ! which pacman >/dev/null; then
-    echo "Your not on an Arch or an Arch based distro, you can not use these dotfiles."
-    exit 1
+  echo "Your not on an Arch or an Arch based distro, you can not use these dotfiles."
+  exit 1
 fi
 
 if ! pacman -Q base-devel >/dev/null; then
-    sudo pacman -S base-devel
+  sudo pacman -S base-devel
 fi
 
 if ! git >/dev/null; then
-    sudo pacman -S git
+  sudo pacman -S git
 fi
 
-git clone https://github.com/lazikiri/hyprland-dots "$HOME/.dotfiles"
+git clone https://github.com/lazikiri/hyprland-dots "$HOME/.config/"
 
 if ! which paru >/dev/null; then
-    rm -rf /tmp/paru
-    git clone https://aur.archlinux.org/paru.git /tmp/paru
-    cd /tmp/paru
-    makepkg -si
+  rm -rf /tmp/paru
+  git clone https://aur.archlinux.org/paru.git /tmp/paru
+  cd /tmp/paru
+  makepkg -si
 fi
 
-paru -S --needed --noconfirm hyprland alacritty waybar ttf-firacode-nerd python-pywal16-git pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber dunst hyprpolkitagent fastfetch eza rofi swww xdg-desktop-portal xdg-desktop-portal-hyprland hyprshot hyprpicker cliphist wl-clipboard wiremix yazi hyprlock fzf bat mpv stow zoxide
-
-cd "$HOME/.dotfiles"
-stow */
+paru -S --needed --noconfirm hyprland waybar ttf-firacode-nerd python-pywal16-git pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber dunst hyprpolkitagent rofi swww xdg-desktop-portal xdg-desktop-portal-hyprland hyprshot cliphist wl-clipboard wiremix yazi hyprlock
+eza fastfetch fzf zoxide
 
 echo "Please reboot your system."
