e11471a0a8aca83d2b3b6894328d795cc57377f8
lazikiri <lazikiri@users.noreply.github.com>
2025-10-16 18:07:06 -0400

    Added Matugen, removed themes

--- .config/rofi/scripts/wallpaper-selector.sh
+++ .config/rofi/scripts/wallpaper-selector.sh
@@ -10,5 +10,6 @@ if [ ! -d "$HOME/.config/swww" ]; then
     mkdir "$HOME/.config/swww"
 fi
 
+rm "$HOME/.config/swww/current"
 ln -sfn "$HOME/Pictures/Wallpapers/$SELECTED_WALLPAPER" "$HOME/.config/swww/current"
-swww img --transition-type grow --transition-pos $(hyprctl cursorpos | tr -d ' ') --invert-y --transition-fps 60 ~/.config/swww/current
+matugen image ~/.config/swww/current
